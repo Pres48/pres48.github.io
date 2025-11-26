7428e0fcc60235a8dd994bc267c7ad06979120c8
Pres48 <prestonhagerty1@gmail.com>
2025-11-26 14:04:28 -0500

    Create supabase.js

---
+++ dayfacts/supabase.js
@@ -0,0 +1,6 @@
+// TODO: REPLACE THESE WITH YOUR SUPABASE PROJECT VALUES
+const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
+const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+
+// Create Supabase client
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
